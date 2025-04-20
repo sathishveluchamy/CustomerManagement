373e8f3a05e31f2a9545960a47e0ca89d1430b9d
sathishveluchamy <sathish.veluchamy@gmail.com>
2025-04-20 17:55:51 +1200

    Added XML comments for API controller

--- CustomerManagement.API/Controllers/CustomerController.cs
+++ CustomerManagement.API/Controllers/CustomerController.cs
@@ -4,23 +4,43 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CustomerManagement.API.Controllers
 {
+    /// <summary>
+    /// API controller for managing customer data.
+    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     public class CustomerController : ControllerBase
     {
         private readonly ICustomerService _service;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomerController"/> class.
+        /// </summary>
+        /// <param name="service">Customer service instance.</param>
         public CustomerController(ICustomerService service)
         {
             _service = service;
         }
 
+        /// <summary>
+        /// Retrieves all customers.
+        /// </summary>
+        /// <returns>A list of customers.</returns>
         [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<CustomerDto>), 200)]
         public async Task<IActionResult> GetAll()
         {
             return Ok(await _service.GetAllAsync());
         }
 
+        /// <summary>
+        /// Retrieves a customer by ID.
+        /// </summary>
+        /// <param name="id">The ID of the customer to retrieve.</param>
+        /// <returns>The customer with the specified ID.</returns>
         [HttpGet("{id}")]
+        [ProducesResponseType(typeof(CustomerDto), 200)]
+        [ProducesResponseType(404)]
         public async Task<IActionResult> Get(Guid id)
         {
             var customer = await _service.GetByIdAsync(id);
@@ -29,7 +49,13 @@ namespace CustomerManagement.API.Controllers
             return Ok(customer);
         }
 
+        /// <summary>
+        /// Creates a new customer.
+        /// </summary>
+        /// <param name="dto">The customer data transfer object.</param>
+        /// <returns>The newly created customer.</returns>
         [HttpPost]
+        [ProducesResponseType(typeof(CustomerDto), 201)]
         public async Task<IActionResult> Post([FromBody] CustomerDto dto)
         {
             await _service.AddAsync(dto);
